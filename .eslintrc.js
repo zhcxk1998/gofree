00cdcff7a57ef5f81a58b135810ce97cc8f5ba00
zhcxk1998 <464203147@qq.com>
2019-03-20 13:58:20 +0800

    chore: 引入eslint配置.

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "parser": "babel-eslint",
+    "extends": "airbnb",
+    "rules": {
+        "react/jsx-filename-extension": 0,
+        "react/prop-types": 0,
+        "react/sort-comp": 0,
+        "react/prefer-stateless-function": 0,
+        "no-extra-boolean-cast": 0,
+        "class-methods-use-this": "off",
+    }
+};
--- webpack.common.js
+++ webpack.common.js
@@ -1,62 +1,63 @@
-const path = require('path')
-const root = __dirname
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const webpack = require('webpack')
-const StringReplacePlugin = require('string-replace-webpack-plugin')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const path = require('path');
+
+const root = __dirname;
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const StringReplacePlugin = require('string-replace-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-    resolve: {
-        extensions: ['.js', '.jsx', '.css']
-    },
-    module: {
-        rules: [
-            {
-                test: /(\.jsx|\.js)$/,
-                use: ['babel-loader'],
-                exclude: /node_modules/
-            },
-            {
-                test: /\.css$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: 'style-loader',
-                    use: ['css-loader?minimize']
-                })
-            },
-            {
-                test: /\.(png|jpg|gif|mp4)$/,
-                use: ['file-loader']
-            },
-            {
-                test: /\.(woff|woff2|ttf|eot|ogg|svg)$/,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            limit: 8192
-                        }
-                    }
-                ]
-            },
-            {
-                test: /semantic\.min\.css$/,
-                loader: StringReplacePlugin.replace({
-                    replacements: [{
-                        pattern: /https\:\/\/fonts\.googleapis\.com[^)']+/ig,
-                        replacement: function(match, p1, offset, string) {
-                            return 'data:text/css,*{}'
-                        }
-                    }]
-                })
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'gofree',
-            template: path.resolve(root, 'src/index.html')
+  resolve: {
+    extensions: ['.js', '.jsx', '.css'],
+  },
+  module: {
+    rules: [
+      {
+        test: /(\.jsx|\.js)$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader?minimize'],
         }),
-        new StringReplacePlugin(),
-        new ExtractTextPlugin('common.css')
-    ]
-}
+      },
+      {
+        test: /\.(png|jpg|gif|mp4)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.(woff|woff2|ttf|eot|ogg|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+            },
+          },
+        ],
+      },
+      {
+        test: /semantic\.min\.css$/,
+        loader: StringReplacePlugin.replace({
+          replacements: [{
+            pattern: /https\:\/\/fonts\.googleapis\.com[^)']+/ig,
+            replacement(match, p1, offset, string) {
+              return 'data:text/css,*{}';
+            },
+          }],
+        }),
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'gofree',
+      template: path.resolve(root, 'src/index.html'),
+    }),
+    new StringReplacePlugin(),
+    new ExtractTextPlugin('common.css'),
+  ],
+};
